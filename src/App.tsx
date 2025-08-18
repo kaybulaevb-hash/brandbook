a0131cff89d8972dae68dcb6a5964a0468c58b0c
kaybulaevb-hash <kaybulaevb@gmail.com>
2025-08-18 21:54:02 +0200

    fix: resolve black background issue - remove conflicting body background and recreate App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AnimatedBackground from './components/AnimatedBackground';
 import HomePage from './pages/HomePage';
 import BrandPage from './pages/BrandPage';
 import './App.css';
@@ -8,7 +7,6 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <AnimatedBackground />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/brand/:slug" element={<BrandPage />} />
